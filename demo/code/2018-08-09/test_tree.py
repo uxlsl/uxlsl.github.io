a66575ad57e64d90a849dcc17ed190324b2872b4
lsl <songlin.lin@yundata.com>
2018-08-10 06:31:01 +0800

    完善测试用例

--- demo/code/2018-08-09/test_tree.py
+++ demo/code/2018-08-09/test_tree.py
@@ -8,8 +8,12 @@ def test_isSame():
     n4 = Node(None, n2, 1) 
     n5 = Node(n1, n2, 3) 
     n6 = Node(n1, n2, 3) 
+    n7 = Node(n1, n4, 3) 
+    n8 = Node(n4, n2, 3) 
     assert n1.IsSame(n2) == True
     assert n1.IsSame(None) == False
     assert n1.IsSame(n3) == False
     assert n3.IsSame(n4) == True
     assert n5.IsSame(n6) == True
+    assert n5.IsSame(n7) == False
+    assert n5.IsSame(n8) == False
