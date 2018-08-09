e65e6d580327e17fd07ab09d0729efdc1485e366
lsl <songlin.lin@yundata.com>
2018-08-10 06:25:03 +0800

    修改一些小错误

--- demo/code/2018-08-09/send_anwser.py
+++ demo/code/2018-08-09/send_anwser.py
@@ -2,7 +2,6 @@ import requests
 
 code = open('./tree.py', 'r').read()
 info = {'username': '林松龄', 'tel': '18613077154', 'code': code}
-
-r = requests.post('http://172.104,90.126:13000/answer', json=info)
+r = requests.post('http://172.104.90.126:13000/answer', json=info)
 print(r.text)
 print(r.json())
